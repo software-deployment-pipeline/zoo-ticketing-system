e0be2292fd10530ee44b90e521c11d567ec693af
MattAxel4 <143594740+MikeKuntol@users.noreply.github.com>
2024-10-14 18:53:20 +1100

    testing jenkins docker testserver

--- zoo-ticketing.py
+++ zoo-ticketing.py
@@ -48,7 +48,7 @@ class ZooTicketing():
 def main():
     zoo_ticketing = ZooTicketing()
     buy_more = True
-    print("Hello my homie, welcome to Swinburne Zoo. Please select ticket.")
+    print("Hello my homie testing test server welcome to Swinburne Zoo. Please select ticket.")
     print()
 
     while buy_more:
