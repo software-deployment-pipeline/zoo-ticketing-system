25d81b7a10554133daf48ffcf0bbfac8450b7958
MattAxel4 <143594740+MikeKuntol@users.noreply.github.com>
2024-10-15 00:35:51 +1100

    test26

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -6,9 +6,6 @@ class TestZooTicketing(unittest.TestCase):
         # A simple test that always passes
         self.assertEqual(1, 1)
 
-    def test_fail_example(self):
-        # A test that will fail (you can comment this out if you don't want to see a failure)
-        self.assertEqual(1, 2)
 
 if __name__ == '__main__':
     unittest.main()
